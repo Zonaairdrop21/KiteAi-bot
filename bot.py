2961b7d38c7cfaac4ae061b6f9c3160bbddaaa50
Zonaairdrop21 <155278715+Zonaairdrop21@users.noreply.github.com>
2025-08-03 20:22:54 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -108,55 +108,20 @@ class KiteAi:
         ]''')
         self.SWAP_ROUTER = "0x04CfcA82fDf5F4210BC90f06C44EF25Bf743D556"
         self.USDT_CONTRACT = "0x0fF5393387ad2f9f691FD6Fd28e07E3969e27e63"
-        self.WKITE_CONTRACT = "0x0BBB7293c08dE4e62137a557BC40bc12FA1897d6"
+        self.WKITE_CONTRACT = "0x3bC8f037691Ce1d28c0bB224BD33563b49F99dE8"
         self.SWAP_ABI = json.loads('''[
-  {
-    "name": "initiate",
-    "type": "function",
-    "stateMutability": "payable",
-    "inputs": [
-      { "name": "token", "type": "address" },
-      { "name": "amount", "type": "uint256" },
-      {
-        "name": "instructions",
-        "type": "tuple",
-        "components": [
-          { "name": "sourceId", "type": "uint256" },
-          { "name": "receiver", "type": "address" },
-          { "name": "payableReceiver", "type": "bool" },
-          { "name": "rollbackReceiver", "type": "address" },
-          { "name": "rollbackTeleporterFee", "type": "uint256" },
-          { "name": "rollbackGasLimit", "type": "uint256" },
-          {
-            "name": "hops",
-            "type": "tuple[]",
-            "components": [
-              { "name": "action", "type": "uint8" },
-              { "name": "requiredGasLimit", "type": "uint256" },
-              { "name": "recipientGasLimit", "type": "uint256" },
-              { "name": "trade", "type": "bytes" },
-              {
-                "name": "bridgePath",
-                "type": "tuple",
-                "components": [
-                  { "name": "bridgeSourceChain", "type": "address" },
-                  { "name": "sourceBridgeIsNative", "type": "bool" },
-                  { "name": "bridgeDestinationChain", "type": "address" },
-                  { "name": "cellDestinationChain", "type": "address" },
-                  { "name": "destinationBlockchainID", "type": "bytes32" },
-                  { "name": "teleporterFee", "type": "uint256" },
-                  { "name": "secondaryTeleporterFee", "type": "uint256" }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ],
-    "outputs": []
-  }
-]''')
-
+            {
+                "name": "initiate",
+                "type": "function",
+                "stateMutability": "payable",
+                "inputs": [
+                    { "name": "token", "type": "address" },
+                    { "name": "amount", "type": "uint256" },
+                    { "name": "instructions", "type": "tuple" }
+                ],
+                "outputs": []
+            }
+        ]''')
         self.TESTNET_API = "https://testnet.gokite.ai"
         self.BRIDGE_API = "https://bridge-backend.prod.gokite.ai"
         self.NEO_API = "https://neo.prod.gokite.ai/v2"
